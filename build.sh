5187d0300dc0558be74cb8fe5391ed19695707e7
Eric Hanson <eric@aquameta.com>
2015-04-15 12:21:46 -0700

    bundle load in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # dropdb aquameta
 createdb aquameta
-cat core/0*/0*.sql  | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
+cat core/0*/*.sql  | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
 cat scripts-enabled/*.sql | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
 cat bundles-enabled/*.sql | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
+echo "select bundle.checkout(c.id) from bundle.commit c join bundle.bundle b on b.head_commit_id = c.id;" | psql aquameta
