c2b63e34431b6b75fcf1ef78e2793f66cf2e6d1e
Mickey Burks <mickey@aquameta.com>
2016-01-08 17:41:55 +0000

    Readable fields

--- core/002-filesystem/fs_fdw/fs_fdw/__init__.py
+++ core/002-filesystem/fs_fdw/fs_fdw/__init__.py
@@ -1,12 +1,16 @@
 from multicorn import ForeignDataWrapper
 import os
+import stat
+from time import ctime
+from grp import getgrgid
+from pwd import getpwuid
 
 class FilesystemForeignDataWrapper(ForeignDataWrapper):
     def __init__(self, options, columns):
         self.col_map={
             'permissions': 'st_mode',
-            'link': 'st_nlink:',
-            'user': 'st_uid',
+            'links': 'st_nlink',
+            'owner': 'st_uid',
             'group': 'st_gid',
             'size': 'st_size',
             'last_mod': 'st_mtime' }
@@ -26,15 +30,32 @@ class FilesystemForeignDataWrapper(ForeignDataWrapper):
         for filename in os.listdir(path):
             f=os.stat(path + '/' + filename)
             row={}
-            for column_name in self.columns:
+            for column_name in columns:
                 if column_name == 'id' or column_name == 'path':
                     row[column_name] = path #+ '/' + filename
                 elif column_name == 'name':
                     row[column_name] = filename
                 elif column_name == 'content':
                     row[column_name] = 'content'
+
+                elif column_name == 'parent_id' or column_name == 'directory_id':
+                    if path == '/':
+                        row[column_name] = None
+                    else:
+                        row[column_name] = '/'.join( path.split('/')[:-1] )
+
+                elif column_name == 'group':
+                    row[column_name] = getgrgid( getattr(f, self.col_map[column_name]) ).gr_name
+                elif column_name == 'owner':
+                    row[column_name] = getpwuid( getattr(f, self.col_map[column_name]) ).pw_name
+                elif column_name == 'permissions':
+                    row[column_name] = oct( stat.S_IMODE( getattr(f, self.col_map[column_name]) ) )
+                elif column_name == 'last_mod':
+                    row[column_name] = ctime( getattr(f, self.col_map[column_name]) )
+
                 elif column_name in self.col_map:
-                    row[column_name] = getattr(f, self.col_map[column_name])
+                        row[column_name] = getattr(f, self.col_map[column_name])
+
                 else:
                     row[column_name] = None
             yield row
