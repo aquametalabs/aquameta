0c88a2c31c18b17def866d872069990aaa8bf3c9
Mickey Burks <mickey@aquameta.com>
2016-01-09 22:57:56 +0000

    Read file and rethink quals

--- core/002-filesystem/fs_fdw/fs_fdw/__init__.py
+++ core/002-filesystem/fs_fdw/fs_fdw/__init__.py
@@ -29,46 +29,82 @@ class FilesystemForeignDataWrapper(ForeignDataWrapper):
             if (qual.field_name == 'path' or qual.field_name == 'id') and qual.operator == '=':
                 path = qual.value
 
+	    elif (qual.field_name == 'directory_id' or qual.field_name == 'parent_id') and qual.operator == '=':
+		path = qual.value
+
+
+
+	# If path is not supplied, return empty set
         if path is None:
             yield {}
             return
 
-	# Regular file
-        if self.type == 'file' and stat.S_ISREG( os.stat(path).st_mode ) and 'content' in columns:
-	    row={}
-	    with open(path, 'r') as infile:
-		row['content'] = infile.read().replace('\n', '')
+
+	# Remove last /
+	path = path[:-1] if path[-1] == '/' else path
+	# If no leading /, change to absolute path
+	path = path if path[0] == '/' else os.path.abspath(path)
+
+
+        # If path is a regular file
+        if stat.S_ISREG( os.stat(path).st_mode ):
+	    filename=path.split('/')[-1]
+	    path='/'.join(path.split('/')[:-1])
+
+            row=self.get_file_stat(filename, path, columns)
+
+            # If 'file' table, get content
+            if self.type == 'file' and stat.S_ISREG( os.stat(path + '/' + filename).st_mode ) and 'content' in columns:
+                with open(path + '/' + filename, 'r') as infile:
+                # import codecs
+                # with codecs.open(path, 'r', 'ascii') as infile:
+                    row['content'] = infile.read().replace('\n', '')
+                    #row['content'] = infile.read().decode('utf-8').encode('ascii', 'ignore')
+
 	    yield row
-	    return
-	    
-	# Directory
-        for filename in os.listdir(path):
-            f=os.stat(path + '/' + filename)
-            row={}
-            for column_name in columns:
-                if column_name == 'id' or column_name == 'path':
+
+	# Path is a directory
+	else:
+            for filename in os.listdir(path):
+	        # Get stats for path
+	        row=self.get_file_stat(filename, path, columns)
+	        yield row
+
+
+    def get_file_stat(self, filename, path, columns):
+	#f=os.stat(path) if self.type == 'file' else os.stat(path + '/' + filename)
+	f=os.stat(path + '/' + filename)
+	row={}	
+        for column_name in columns:
+            if column_name == 'id' or column_name == 'path':
+                row[column_name] = path + '/' + filename
+            elif column_name == 'name':
+                row[column_name] = filename
+            elif column_name == 'content':
+	        row[column_name] = '...'
+
+            elif column_name == 'parent_id' or column_name == 'directory_id':
+	        if path == '/' or path == '':
+		    # Root directory has no parent
+	    	    row[column_name] = None
+	        else:
                     row[column_name] = path
-                elif column_name == 'name':
-                    row[column_name] = filename
-                elif column_name == 'content':
-		    row[column_name] = '...'
+                    #row[column_name] = '/' + '/'.join( path.split('/')[1:-1] )
 
-                elif column_name == 'parent_id' or column_name == 'directory_id':
-		    row[column_name] = None if path == '/' else '/'.join( path.split('/')[:-1] )
+            elif column_name == 'group':
+                row[column_name] = getgrgid( getattr(f, self.col_map[column_name]) ).gr_name
+            elif column_name == 'owner':
+                row[column_name] = getpwuid( getattr(f, self.col_map[column_name]) ).pw_name
+            elif column_name == 'permissions':
+                row[column_name] = oct( stat.S_IMODE( getattr(f, self.col_map[column_name]) ) )
+            elif column_name == 'last_mod':
+                row[column_name] = ctime( getattr(f, self.col_map[column_name]) )
 
-                elif column_name == 'group':
-                    row[column_name] = getgrgid( getattr(f, self.col_map[column_name]) ).gr_name
-                elif column_name == 'owner':
-                    row[column_name] = getpwuid( getattr(f, self.col_map[column_name]) ).pw_name
-                elif column_name == 'permissions':
-                    row[column_name] = oct( stat.S_IMODE( getattr(f, self.col_map[column_name]) ) )
-                elif column_name == 'last_mod':
-                    row[column_name] = ctime( getattr(f, self.col_map[column_name]) )
+            elif column_name in self.col_map:
+                    row[column_name] = getattr(f, self.col_map[column_name])
 
-                elif column_name in self.col_map:
-                        row[column_name] = getattr(f, self.col_map[column_name])
+            else:
+                row[column_name] = None
 
-                else:
-                    row[column_name] = None
-            yield row
+        return row
 
