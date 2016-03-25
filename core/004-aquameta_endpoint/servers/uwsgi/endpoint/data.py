80b698ac1672941593dfb5da3d71b597ca5b033b
Mickey Burks <mickey@aquameta.com>
2016-03-25 21:30:09 +0000

    New request and handlers

--- core/004-aquameta_endpoint/servers/uwsgi/endpoint/data.py
+++ core/004-aquameta_endpoint/servers/uwsgi/endpoint/data.py
@@ -6,25 +6,36 @@ from os import environ
 
 import json
 
+# For logging
+import logging, sys
+logger = logging.getLogger('events')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
 @responder
 def application(env, start_response):
     request = Request(env)
 
     try:
         with map_errors_to_http(), cursor_for_request(request) as cursor:
+            logging.info('query %s, post %s'% (json.dumps(request.args.to_dict(flat=True)), request.data.decode('utf8') if request.data else 'null'))
+                # will be
+                # select status, message, response as json
             cursor.execute('''
                 select status, message, data2 as json
                 from endpoint.request(%s, %s, %s::json, %s::json)
             ''', (
-                request.method,
-                request.path,
-                json.dumps(request.args.to_dict(flat=True)),
+                request.method, # verb - GET | POST | PATCH | PUT | DELETE ...
+                request.path, # path - the full path including leading slash but without query string
+                json.dumps(request.args.to_dict(flat=True)), # args - "url parameters", aka parsed out query string, converted to a json string
                 request.data.decode('utf8') if request.data else 'null'
             ))
 
             row = cursor.fetchone()
             # return Response('Hello World!')
 
+            # TODO?
+            # How come status and message are not used here?
             return Response(
                 row.json,
                 content_type="application/json"
