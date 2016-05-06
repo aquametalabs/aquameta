a2d34ccab320bf6cc492effff488c3939b9f2796
Mickey Burks <mickey@aquameta.com>
2016-05-06 20:46:11 +0000

    Dependency loading with System.js, post_js function works

--- core/007-widget/js/widget.js
+++ core/007-widget/js/widget.js
@@ -93,27 +93,21 @@ widget('/use/edit/integer', { value: 5 })
 * Company: http://aquameta.com/
 * Project: http://blog.aquameta.com/
 ******************************************************************************/
-(function(window, $, doT, AQ, undefined) {
+define(['/doT.min.js', '/Datum.js'], function(doT, AQ, undefined) {
+//(function(window, $, doT, AQ, uuid, undefined) {
     'use strict';
-
-    var retrieve_promises = {};
-    var prepare_promises = {};
-    widget.namespaces = {};
 
     doT.templateSettings.strip = false;
 
-    var caches = {};
-    if('WeakMap' in window) {
-        caches.input_defaults = new WeakMap();
-        caches.pre_js = new WeakMap();
-        caches.html = new WeakMap();
-        caches.css = new WeakMap();
-        caches.post_js = new WeakMap();
-    }
+    var retrieve_promises = {};
+    var prepare_promises = {};
+    var namespaces = {};
 
 
+    AQ.Widget = {};
 
-    function widget( name, input, callback ) {
+
+    AQ.Widget.load = function ( name, input, callback ) {
 
         if (!name || typeof name != 'string') {
             throw "in call to widget, name argument is invalid or missing";
@@ -141,41 +135,39 @@ widget('/use/edit/integer', { value: 5 })
                 context.name = name_parts[1];
             }
 
-            //console.log(context);
-
             // Go get this widget - retrieve_promises don't change for calls to the same widget - they are cached by the widget name
             if (context.name in retrieve_promises) {
                 var retrieve_promise = retrieve_promises[context.name];
             }
             else {
-                var retrieve_promise = widget.retrieve(context.namespace, context.name);
+                var retrieve_promise = retrieve(context.namespace, context.name);
             }
         }
 
         // Prepare and render the widget - each prepare_promise is unique because inputs are different - they are cached by the unique uuid created for the context
-        prepare_promises[context.id] = widget.prepare(retrieve_promise, context, callback);
+        prepare_promises[context.id] = prepare(retrieve_promise, context, callback);
 
         // Return script that calls swap
         return '<script id="widget-stub_' + context.id  + '" data-widget_id="' + context.id + '">' +
-                  'widget.swap($("#widget-stub_' +  context.id  + '"), "' + context.id + '");'  + 
+                  'AQ.Widget.swap($("#widget-stub_' +  context.id  + '"), "' + context.id + '");'  + 
                '</script>';
 
     }
 
 
 
-    widget.import = function( bundle_name, namespace, endpoint ) {
+    AQ.Widget.import = function( bundle_name, namespace, endpoint ) {
 
         // TODO: This will have to be a sweet lookup of widget through bundle and head_db_stage
-        widget.namespaces[namespace] = endpoint.schema('widget').table('widget').rows();
+        namespaces[namespace] = endpoint.schema('widget').table('widget').rows();
 
     };
 
 
 
-    widget.retrieve = function( namespace, name ) {
+    function retrieve( namespace, name ) {
 
-        return widget.namespaces[namespace]
+        return namespaces[namespace]
             .then(function(rows) {
 
                 // Get the correct widget
@@ -205,12 +197,34 @@ widget('/use/edit/integer', { value: 5 })
 
                         }), // This may need .bind(this)
                     row.related_rows('id', 'widget.widget_dependency_js', 'widget_id')
-                        .then(function(dep_js) {
+                        .then(function(deps_js) {
 
-                            if (!dep_js) {
+                            if (!deps_js) {
                                 return null;
                             }
-                            return dep_js.related_row('dependency_js_id', 'widget.dependency_js', 'id')
+                            return deps_js.related_rows('dependency_js_id', 'widget.dependency_js', 'id');
+
+                        }).then(function(deps) {
+
+                            if (!deps) {
+                                return [];
+                            }
+
+                            var base_url = deps.relation.schema.database.url;
+                            return Promise.all(
+
+                                deps.rows.map(function(dep) {
+                                    return System.import(base_url + '/field/widget/dependency_js/' + dep.row.id + '/content').then(function(dep_module) {
+                                        console.log('my module', dep_module);
+                                        return {
+                                            url: base_url + '/field/widget/dependency_js/' + dep.row.id + '/content',
+                                            name: dep.row.variable || 'non_amd_module',
+                                            /* TODO: This value thing is a hack. For some reason, jwerty doesn't load properly here */
+                                            value: typeof dep_module == 'object' ? dep_module[Object.keys(dep_module)[0]] : dep_module
+                                        };
+                                    });
+                                })
+                            );
 
                         })
                 ]);
@@ -220,26 +234,28 @@ widget('/use/edit/integer', { value: 5 })
 
 
 
-    widget.prepare = function( retrieve_promise, context, callback ) {
+    function prepare( retrieve_promise, context, callback ) {
         //console.log('args to prepare', arguments);
         return retrieve_promise.then(function( widget_data ) {
 
             //console.log('retrieve_promise resolved', widget_data);
+            var [ widget_row, inputs, views, deps_js ] = widget_data;
 
-            var widget_row = widget_data[0];
-            var inputs = widget_data[1];
-            var views = widget_data[2];
-            var deps_js = widget_data[3];
+            context = Object.assign({
+                    db: widget_row.relation.schema.database,
+                    endpoint: widget_row.relation.schema.database
+                }, context);
 
             // Do some preparation
             // Process inputs
             if (inputs != null) {
+
                 for (var i = 0; i < inputs.length; i++) {
                     var input_name = inputs[i].get('name');
 
                     if(typeof context[input_name] != 'undefined') {
                         if(inputs[i].get('optional')) {
-                            context[input_name] = cached_get_or_create('input_defaults', inputs[i], function() {
+                            context[input_name] = (function() {
                                 var default_code = inputs[i].get('default_value');
 
                                 try {
@@ -254,7 +270,7 @@ widget('/use/edit/integer', { value: 5 })
                                     console.error("Widget default eval failure", input_row.field('default_value').value);
                                     throw e;
                                 }
-                            });
+                            }());
                         }
                         else {
                             error('Missing required input ' + input_name, context.name, 'Inputs');
@@ -270,21 +286,8 @@ widget('/use/edit/integer', { value: 5 })
                 }
             }
 
-            // Deps?
-            if (deps_js != null) {
-                for (var i = 0; i < deps_js; i++) {
-                    try {
-                        var dep_js = eval(deps_js[i].get('content'));
-                        // Cache this
-                    }
-                    catch(e) {
-                        error(e, context.name, 'DEP_JS Could not load dep' + dep_js[i].get('name') + ':' + dep_js[i].get('version'));
-                    }
-                }
-            }
-
-            var rendered_widget = widget.render(widget_row, context);
-            var post_js_function = widget.create_post_js_function(widget_row, context, deps_js);
+            var rendered_widget = render(widget_row, context);
+            var post_js_function = create_post_js_function(widget_row, context, deps_js);
 
             // Return rendered widget and post_js function
             return {
@@ -299,14 +302,11 @@ widget('/use/edit/integer', { value: 5 })
 
 
 
-    widget.render = function( widget_row, context ) {
-
+    function render( widget_row, context ) {
         //console.log('render params', arguments);
 
         // Create html template
-        var html_template = cached_get_or_create('html', widget_row, function() {
-            return doT.template(widget_row.get('html') || '');
-        });
+        var html_template = doT.template(widget_row.get('html') || '');
 
         // Compile html template
         try {
@@ -333,9 +333,7 @@ widget('/use/edit/integer', { value: 5 })
         if (widget_row.get('css') != null && $('style[data-widget="' + context.name + '"]').length == 0) {
 
             // Create css template
-            var css_template = cached_get_or_create('css', widget_row, function() {
-                return doT.template(widget_row.get('css') || '');
-            });
+            var css_template = doT.template(widget_row.get('css') || '');
 
             // Try to run css template
             try {
@@ -353,109 +351,67 @@ widget('/use/edit/integer', { value: 5 })
 
 
 
-    widget.create_post_js_function = function( widget_row, context, deps_js ) {
-        // Create post_js function with deps, inputs, views, required deps, sourceMap
+    function create_post_js_function( widget_row, context, deps_js ) {
 
-        //return function() { console.log('post_js called'); };
+        var context_keys = Object.keys(context).sort();
 
-        // This may become a common function between all calls to the same widget
-        // Then inputs will all come in on the input argument
+        // Get context values
+        var context_vals = [];
+        for (var i = 0; i < context_keys.length; i++) {
+            context_vals.push( context[context_keys[i]] );
+        }
 
-        // First we'll do proof of tech by supporting the old argument method
+        var dep_names = [];
+        var dep_values = [];
+        if (deps_js != null) {
+            deps_js.forEach(function(dep_js) {
+                dep_names.push(dep_js.name);
+                dep_values.push(dep_js.value);
+            });
+        }
 
-        var post_js = cached_get_or_create('post_js', widget_row, function() {
+        try {
+            /*
+            var post_js = Function.apply(context, context_keys.concat([
+                    '\n\nvar w = $("#"+id);\n\n' +
+                    widget_row.get('post_js') +
+                    '\n\n//# sourceURL=' + widget_row.get('id') + '/' + widget_row.get('name') + '/post_js\n\n'
+                ])
+            );
+            */
 
-            var context_keys = Object.keys(context).sort();
-
-            try {
-                var js_code = Function.apply(null, context_keys.concat([
-                        '\n\nvar w = $("#"+id);\n\n' +
-                        widget_row.get('post_js') +
-                        '\n\n//# sourceURL=' + widget_row.get('id') + '/' + widget_row.get('name') + '/post_js\n\n'
-                    ])
-                );
-                /*
-                var js_code = Function(
+            /*
+            * Creating an script that looks like this
+            * function(dep1_name, dep2_name, ...) {
+            *   function(input1, input2) {
+            *       post_js
+            *   }.apply(this.this.context_vals);
+            * }.apply(this, this.dep_vals);
+            */
+            var post_js = Function(
+                '(function(' + dep_names.join(',') + ') { \n' +
                     '(function(' + context_keys.join(',') + ') { \n' +
                         'var w = $("#"+id);\n\n' +
                         widget_row.get('post_js') +
                         '\n//# sourceURL=' + widget_row.get('id') + '/' + widget_row.get('name') + '/post_js\n' +
-                    '}).apply(null, this.context_vals);'
-                );
-                */
-            }
-            catch(e) {
-                error(e, widget_row.get('name'), 'Creating post_js function');
-            }
-
-            //console.log('my js code', js_code);
-
-            // Pass inputs into js_code function
-
-            var dep_keys = [];
-            if (deps_js != null) {
-                dep_keys.concat[deps_js];
-                /*
-                for (var i = 0; i < deps_js.length; i++) {
-                    dep_keys.push(deps_js[i]);
-                    //dep_keys.push(deps_js[i].get('variable') || 'non_amd_module');
-                }
-                */
-            }
-
-            Object.assign(dep_keys, {
-                '$': $,
-                db: widget_row.relation.schema.database,
-                endpoint: widget_row.relation.schema.database, // to be removed
-                widget: widget,
-                AQ: AQ
-            });
-
-            dep_keys.sort();
-
-            /*
-            try {
-                var post_js_with_inputs = Function.apply(null, Object.keys(context).sort().concat([ js_code ]));
-                    //'(function() { \n' + Object.keys(// _.keys(context).sort().join(', ') + ') { \n' +
-                        //js_code +
-                    //'\n }).apply(null, this.context_vals);'
-                //]));
-            }
-            catch(e) {
-                error(e, widget_row.get('name'), 'Creating post_js function');
-            }
-            */
-
-            return {
-                fn: js_code,
-                context_keys: context_keys,
-                dep_keys: dep_keys
-            };
-
-        });
-
-        // Get context values
-        var context_vals = [];
-        for (var i = 0; i < post_js.context_keys.length; i++) {
-            context_vals.push( context[post_js.context_keys[i]] );
+                    '}).apply(this, this.context_vals);' +
+                '}).apply(this, this.dep_values);'
+            ).bind({ context_vals: context_vals, dep_values: dep_values });
+        }
+        catch(e) {
+            error(e, widget_row.get('name'), 'Creating post_js function');
         }
 
-        // Get dependency values
-        var dep_vals = [];
-        for (var i = 0; i < post_js.dep_keys.length; i++) {
-            dep_vals.push( dependencies[post_js.dep_keys[i]] );
-        }
-
-        return function() { post_js.fn.apply(null, context_vals) }.bind(this);
+        //console.log('my js code', post_js);
+        return post_js;
 
     };
 
 
 
-    widget.swap = function( $element, id ) {
+    AQ.Widget.swap = function( $element, id ) {
 
         prepare_promises[id].then(function(rendered_widget) {
-
             //console.log('prepare_promise resolved', rendered_widget);
 
             // Replace stub
@@ -481,21 +437,18 @@ widget('/use/edit/integer', { value: 5 })
 
 
 
-    widget.purge_cache = function( widget_name ) {
-        return widget.retrieve(widget_name).then(function(resources) {
+    AQ.Widget.purge_cache = function( widget_name ) {
+        return retrieve(widget_name).then(function(resources) {
             delete retrieve_promises[widget_name];
-            //delete prepare_promises[widget_name];
-
-            if('WeakMap' in window) {
-                caches.input_defaults.delete(resources.widget_row)
-                caches.pre_js.delete(resources.widget_row);
-                caches.html.delete(resources.widget_row);
-                caches.css.delete(resources.widget_row);
-                caches.post_js.delete(resources.widget_row);
-                console.log("Cache cleared.");
-            }
         });
     };
+
+
+
+    function error( err, widget_name, step_name ) {
+        console.error("widget('" + widget_name + "', ...) " + step_name + " failed!");
+        throw err;
+    }
 
 
 
@@ -510,30 +463,13 @@ widget('/use/edit/integer', { value: 5 })
 
 
 
-    function error( err, widget_name, step_name ) {
-        console.error("widget('" + widget_name + "', ...) " + step_name + " failed!");
-        throw err;
-    }
-
-
-
-    function cached_get_or_create( cache_name, cache_key, create_func ) {
-        if(cache_name in caches && caches[cache_name].has(cache_key)) {
-            var r = caches[cache_name].get(cache_key);
-        }
-        else {
-            var r = create_func();
-            if(cache_name in caches) caches[cache_name].set(cache_key, r);
-        }
-        return r;
-    }
-
-
-
     /********************************************************************
     COPIED OVER
     ********************************************************************/
-    widget.sync = function(rowlist_promise, container, widget_maker, handlers) {
+    AQ.Widget.load.sync = function(rowlist_promise, container, widget_maker, handlers) {
+
+        console.log('calling widget sync');
+
         if(handlers === undefined) {
             handlers = {};
         }
@@ -559,6 +495,10 @@ widget('/use/edit/integer', { value: 5 })
 
     }
 
-    window.widget = widget;
-}(window, jQuery, doT, AQ));
+    window.widget = AQ.Widget.load;
+    return AQ.Widget.load;
+
+//}(this, jQuery, doT, AQ || {}, uuid));
+
+});
 
