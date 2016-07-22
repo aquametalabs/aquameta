67054734c29dad7596478cc08547a9979e6cceba
Mickey Burks <mickey@aquameta.com>
2016-07-22 14:46:56 +0000

    Complete semantic widget functions

--- core/008-semantics/000-semantics.sql
+++ core/008-semantics/000-semantics.sql
@@ -161,23 +161,26 @@ create or replace function semantics.relation_widget (
     default_bundle text
 ) returns setof widget.widget as
 $$
-    -- Find all the possible widgets referenced in semantics for this relation and purpose
-    select * from (
-        select * from (
-            select w.*
-            from semantics.semantic_relation r
-                join semantics.semantic_relation_purpose rp on rp.id = r.purpose_id
-                join widget.widget w on w.id = r.widget_id
-            where r.id = relation_id
-                and rp.purpose = widget_purpose
-            order by r.priority desc
-            limit 1
-        ) a
-        union
-        select * from widget.bundled_widget(default_bundle, widget_purpose)
-    ) b
-    limit 1;
-$$ language sql;
+begin
+    return query execute 'select ' || (
+        select string_agg(name, ', ')
+        from meta.column
+        where schema_name='widget'
+            and relation_name='widget' ) ||
+    ' from (
+        select w.*, r.priority
+        from semantics.semantic_relation r
+            join semantics.semantic_relation_purpose rp on rp.id = r.purpose_id
+            join widget.widget w on w.id = r.widget_id
+        where r.id = meta.relation_id(' || quote_literal((relation_id::meta.schema_id).name) || ', ' || quote_literal((relation_id).name) || ')
+            and rp.purpose = ' || quote_literal(widget_purpose) ||
+        'union
+        select *, -1 as priority from widget.bundled_widget(' || quote_literal(default_bundle) || ', ' || quote_literal(widget_purpose) || ')
+    ) a
+    order by priority desc
+    limit 1';
+end;
+$$ language plpgsql;
 
 
 
@@ -187,34 +190,39 @@ create or replace function semantics.column_widget (
     default_bundle text
 ) returns setof widget.widget as
 $$
-    -- Find all the possible widgets referenced in semantics for this column or type and purpose
-    select * from (
-        select * from (
-            (select w.*
-            from semantics.semantic_column c
-                join semantics.semantic_column_purpose cp on cp.id = c.purpose_id
-                join widget.widget w on w.id = c.widget_id
-            where c.id = column_id
-                and cp.purpose = widget_purpose
-            order by c.priority desc)
-
-            union
-
-            (select w.*
-            from semantics.semantic_type t
-                join semantics.semantic_column_purpose cp on cp.id = t.purpose_id
-                join widget.widget w on w.id = t.widget_id
-                join meta.column mc on mc.type_id = t.id
-            where mc.id = column_id
-                and cp.purpose = widget_purpose
-            order by t.priority desc)
-        ) a
-        union
-        select * from widget.bundled_widget(default_bundle, widget_purpose)
-    ) b
-    limit 1;
-$$ language sql;
-
+begin
+    return query execute 'select ' || (
+        select string_agg(name, ', ')
+        from meta.column
+        where schema_name='widget'
+            and relation_name='widget' ) ||
+    ' from (
+        select w.*, c.priority, ''c'' as type
+        from semantics.semantic_column c
+            join semantics.semantic_column_purpose cp on cp.id = c.purpose_id
+            join widget.widget w on w.id = c.widget_id
+        where c.id = meta.column_id(' || quote_literal((column_id::meta.schema_id).name) || ', ' ||
+                                         quote_literal((column_id::meta.relation_id).name) || ', ' ||
+                                         quote_literal((column_id).name) || ')
+            and cp.purpose = ' || quote_literal(widget_purpose) ||
+        ' union
+        select w.*, t.priority, ''t'' as type
+        from semantics.semantic_type t
+            join semantics.semantic_column_purpose cp on cp.id = t.purpose_id
+            join widget.widget w on w.id = t.widget_id
+            join meta.column mc on mc.type_id = t.id
+        where mc.id = meta.column_id(' || quote_literal((column_id::meta.schema_id).name) || ', ' ||
+                                         quote_literal((column_id::meta.relation_id).name) || ', ' ||
+                                         quote_literal((column_id).name) || ')
+            and cp.purpose = ' || quote_literal(widget_purpose) ||
+        ' union
+        select *, -1 as priority, ''z'' as type
+        from widget.bundled_widget(' || quote_literal(default_bundle) || ', ' || quote_literal(widget_purpose) || ')
+    ) a
+    order by type asc, priority desc
+    limit 1';
+end;
+$$ language plpgsql;
 
 
 
