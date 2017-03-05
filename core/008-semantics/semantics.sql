8fd659c1d99a76dd97e86e38621aa4d92a6b5604
John Was <janek.jan@gmail.com>
2017-03-05 20:59:21 +0100

    update Dockerfile to use Postgres 9.6

--- core/008-semantics/semantics.sql
+++ core/008-semantics/semantics.sql
@@ -9,27 +9,16 @@ begin;
 
 set search_path=semantics;
 
-/*
-
-doesn't work:
-
-ERROR:  column "list_item_identifier_widget_id" of relation "relation" does not exist
-LINE 1: insert into semantics.relation (id, list_item_identifier_wid...
-
-
-
-
-insert into semantics.relation (id, list_item_identifier_widget_id) values
+insert into semantics.relation (relation_id, widget_id) values
 (
     ( select relation_id from meta.relation_id('semantics', 'relation') ),
     ( select id from widget.widget where name = 'relation_listitem_identifier')
 );
 
-insert into semantics.relation (id, list_item_identifier_widget_id) values
+insert into semantics.relation (relation_id, widget_id) values
 (
     ( select relation_id from meta.relation_id('semantics', 'column') ),
     ( select id from widget.widget where name = 'column_listitem_identifier')
 );
 
-*/ 
 commit;
