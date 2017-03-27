51b265971f84223be4ad9409f73509b779b3c118
Eric Hanson <eric@aquameta.com>
2017-03-26 23:05:22 -0700

    move core semantics and endpoint.mimetypes into bundles, remove from installer

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 # dropdb aquameta
 echo "Creating database ..."
-createdb aquameta # --encoding UNICODE aquameta
-
+createdb aquameta # --encoding UNICODE
 
 echo "Loading requirements ..."
-cat core/requirements.sql | psql -U postgres aquameta
+cat core/requirements.sql | psql aquameta
 
 echo "Loading core/*.sql ..."
 cat core/0*/0*.sql  | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
-
 
 echo "Loading bundles-enabled/*.sql ..."
 cat bundles-enabled/*.sql | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
@@ -16,13 +14,7 @@ cat bundles-enabled/*.sql | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
 echo "Checking out head commit of every bundle ..."
 echo "select bundle.checkout(c.id) from bundle.commit c join bundle.bundle b on b.head_commit_id = c.id;" | psql aquameta
 
-echo "Loading semantics ..."
-cat core/0*/semantics.sql  | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
-
 echo "Loading default permissions ..."
 cat core/004-aquameta_endpoint/default_permissions.sql  | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
-
-echo "Loading mimetypes ..."
-cat core/004-aquameta_endpoint/mimetypes.sql  | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
 
 exit 0
