9a146f0b8d11b8824ccee68599887b5e5fda5ae2
Eric Hanson <eric@aquameta.com>
2017-03-27 12:09:55 -0700

    forgot to ignore function_parameter

--- core/006-bundle/002-metabundle.sql
+++ core/006-bundle/002-metabundle.sql
@@ -12,6 +12,9 @@
  * This bundle contains the version control settings for the version control
  * system itself.  It does things like ignore the bundle schema, so that you
  * can't verson control the version control system.
+ *
+ * We also currently ignore everything in meta, until that can be tested 
+ * and worked out.
  */
 
 begin;
@@ -37,6 +40,12 @@ insert into bundle.ignored_row(bundle_id, row_id) values (
     (select id from bundle.bundle where name='com.aquameta.bundle'),
     meta.row_id('meta','relation','id',meta.relation_id('meta','function')::text)
 );
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','function_parameter')::text)
+);
+
 insert into bundle.ignored_row(bundle_id, row_id) values (
     (select id from bundle.bundle where name='com.aquameta.bundle'),
     meta.row_id('meta','relation','id',meta.relation_id('meta','trigger')::text)
@@ -44,8 +53,107 @@ insert into bundle.ignored_row(bundle_id, row_id) values (
 
 insert into bundle.ignored_row(bundle_id, row_id) values (
     (select id from bundle.bundle where name='com.aquameta.bundle'),
-    meta.row_id('meta','relation','id',meta.relation_id('meta','function_parameter')::text)
+    meta.row_id('meta','relation','id',meta.relation_id('meta','cast')::text)
 );
 
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','column')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','connection')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','constraint_check')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','constraint_unique')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','extension')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','foreign_column')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','foreign_data_wrapper')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','foreign_key')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','foreign_server')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','foreign_table')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','policy')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','policy_role')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','relation')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','role')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','role_inheritance')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','schema')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','table')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','table_privilege')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','type')::text)
+);
+
+insert into bundle.ignored_row(bundle_id, row_id) values (
+    (select id from bundle.bundle where name='com.aquameta.bundle'),
+    meta.row_id('meta','relation','id',meta.relation_id('meta','view')::text)
+);
 
 commit;
