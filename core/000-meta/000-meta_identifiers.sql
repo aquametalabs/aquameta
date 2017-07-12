68eb46cc2480ec4cbcdd12cdd42c91841d5b2216
Eric Hanson <eric@aquameta.com>
2017-07-12 13:14:57 -0700

    start to build out meta utils, for functions like row_exists and row_delete, etc...

--- core/000-meta/000-meta_identifiers.sql
+++ core/000-meta/000-meta_identifiers.sql
@@ -459,20 +459,6 @@ from exec((
 );
 */
 
-create or replace function meta.row_exists(in row_id meta.row_id, out answer boolean) as $$
-    declare
-        stmt text;
-    begin
-        execute 'select (count(*) = 1) from ' || quote_ident((row_id::meta.schema_id).name) || '.' || quote_ident((row_id::meta.relation_id).name) ||
-                ' where ' || quote_ident((row_id.pk_column_id).name) || ' = ' || quote_literal(row_id.pk_value)
-            into answer;
-    exception
-        when others then answer := false;
-
-    end;
-$$ language plpgsql;
-
-
 create function meta.eq(
     leftarg meta.row_id,
     rightarg json
