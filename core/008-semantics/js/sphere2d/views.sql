d218dbef32e72a6d6b80c7c1a4b390d43f0fdc99
Eric Hanson <eric@aquameta.com>
2017-08-15 12:27:57 -0700

    remove semantics.sql files, now in bundles

--- core/008-semantics/js/sphere2d/views.sql
+++
@@ -1,8 +0,0 @@
-drop schema if exists ux cascade;
-create schema ux;
-set search_path=ux;
-create view all_tables_schemas as
-    select s.name || '.' || r.name as name, s.name as schema_name, r.name as relation_name
-        from meta.schema s
-        join meta.relation r on r.schema_id=s.id
-        order by s.name, r.name;
