885c9ccb2f847abc7984e075058458022e984c29
Eric Hanson <eric@aquameta.com>
2017-08-14 18:34:04 -0700

    add --silent option to installer, and upgrade pip so it doesn't complain

--- install.sh
+++ install.sh
@@ -12,11 +12,14 @@ echo "Aquameta 0.1 Installer Script"
 echo "This script should be run on an Ubuntu Linux server instance, 14.04 or greater."
 echo "This code is highly experimental and should NOT be run in a production environment."
 
-read -p "Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if ! [[ $REPLY =~ ^[Yy]$ ]]
+if [ $0 = '--silent' ]
 then
-    exit 1
+    read -p "Are you sure? " -n 1 -r
+    echo    # (optional) move to a new line
+    if ! [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        exit 1
+    fi
 fi
 
 # make sure we're running as root
@@ -24,6 +27,8 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 fi
+
+
 
 # make sure this script is being run in /s/aquameta
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -75,6 +80,7 @@ sudo -u postgres /usr/local/bin/pg_ctl -D /var/lib/postgresql/aquameta -l /var/l
 # Install some cool PostgreSQL extensions and python modules
 pgxn install multicorn
 pgxn install pgtap
+pip install --upgrade pip
 pip install requests fusepy
 
 
