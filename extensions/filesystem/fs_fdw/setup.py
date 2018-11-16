90248a610cffbd925fb4d523c0f3cfcb66928f5a
Eric Hanson <eric@aquameta.com>
2018-11-16 01:16:56 +0000

    modernize the filesystem_fdw a bit.

---
+++ extensions/filesystem/fs_fdw/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='FilesystemForeignDataWrapper',
+    version='0.2.0',
+    packages=['filesystem_fdw',],
+    long_description=open('README.txt').read(),
+)
