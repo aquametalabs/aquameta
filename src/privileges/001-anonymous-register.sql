c1133b25eb73915917cd2c67bd6ba8bf71d07bb9
Eric Hanson <eric@aquameta.com>
2019-04-13 16:31:31 +0000

    privileges: grant temporary select access to anonymous on a couple tables
    ... until we get a sane RLS scheme in place

--- src/privileges/001-anonymous-register.sql
+++ src/privileges/001-anonymous-register.sql
@@ -25,6 +25,7 @@ grant execute on function widget.bundled_widget(text, text) to anonymous;
 -- table privileges
 -- endpoint.resource
 grant select on endpoint.resource to anonymous;
+/*
 alter table endpoint.resource enable row level security;
 create policy anonymous on endpoint.resource for select to anonymous
     using (path in ('/', '/login', '/register', '/confirm', '/system.js','/datum.js','/widget.js', '/jQuery.min.js','/doT.min.js'));
@@ -33,6 +34,7 @@ create policy anonymous on endpoint.resource for select to anonymous
 grant select on endpoint.resource_binary to anonymous;
 alter table endpoint.resource_binary enable row level security;
 create policy anonymous on endpoint.resource_binary for select to anonymous using (false);
+*/
 
 
 -- TODO add security policies on these
@@ -44,12 +46,15 @@ grant select on endpoint.mimetype to anonymous;
 grant select on endpoint.mimetype_extension to anonymous;
 grant select on endpoint.current_user to anonymous;
 grant select on endpoint.column_mimetype to anonymous;
+grant select on endpoint.resource to anonymous;
+grant select on endpoint.resource_binary to anonymous;
 grant select on endpoint.template_route to anonymous;
 grant select on endpoint.template to anonymous;
 grant select on bundle.bundle to anonymous;
 grant select on bundle.tracked_row to anonymous;
 grant select on widget.widget to anonymous;
 grant select on widget.widget_view to anonymous;
+grant select on widget.dependency_js to anonymous;
 grant select on widget.widget_dependency_js to anonymous;
 grant select on widget.input to anonymous;
 
