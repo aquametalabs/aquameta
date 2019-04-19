5ef39d6b8c3de76dfa31c18d3e687254efd29ea0
Eric Hanson <eric@aquameta.com>
2019-04-19 14:48:45 +0000

    bundle: push/pull improvements

--- src/pg-extension/bundle/003-remotes-fdw.sql
+++ src/pg-extension/bundle/003-remotes-fdw.sql
@@ -22,7 +22,7 @@
 -- setup a foreign server to a remote, and import it's bundle schema
 
 
-create or replace function remote_mount (
+create or replace function bundle.remote_mount (
     foreign_server_name text,
     schema_name text,
     host text,
@@ -65,7 +65,7 @@ $$ language plpgsql;
 
 
 
-create or replace function remote_mount( remote_database_id uuid ) returns boolean as $$
+create or replace function bundle.remote_mount( remote_database_id uuid ) returns boolean as $$
     select bundle.remote_mount(
         foreign_server_name,
         schema_name,
@@ -80,7 +80,7 @@ $$ language sql;
 
 
 
-create or replace function remote_unmount( remote_database_id uuid ) returns boolean as $$
+create or replace function bundle.remote_unmount( remote_database_id uuid ) returns boolean as $$
 declare
     _schema_name text;
     _foreign_server_name text;
@@ -94,7 +94,7 @@ $$ language plpgsql;
 
 
 
-create or replace function remote_is_mounted( remote_database_id uuid ) returns boolean as $$
+create or replace function bundle.remote_is_mounted( remote_database_id uuid ) returns boolean as $$
 declare
     _schema_name text;
     _foreign_server_name text;
@@ -116,12 +116,18 @@ $$ language plpgsql;
 --
 -- contains a row for each bundle in a database, containing the "commit" row of each commit in the bundle
 
-create or replace function bundle_commits_array( bundle_relation_id meta.relation_id )
+create or replace function bundle.bundle_commits_array( bundle_relation_id meta.relation_id, bundle_id uuid default null )
 returns table (
     id uuid, name text, head_commit_id uuid, commits json
 )
 as $$
+declare 
+    bundle_filter_stmt text;
 begin
+    bundle_filter_stmt := '';
+    if bundle_id is not null then
+        bundle_filter_stmt := format('where bundle_id = %L', bundle_id);
+    end if;
     return query execute format('
         select
             b.id,
@@ -136,142 +142,79 @@ begin
             )) as commits
         from %I.%I b
             join %I.commit c on c.bundle_id=b.id
+        %s
         group by b.id, b.name, b.head_commit_id
     ',
         (bundle_relation_id::meta.schema_id).name,
         bundle_relation_id.name,
-        (bundle_relation_id::meta.schema_id).name
+        (bundle_relation_id::meta.schema_id).name,
+        bundle_filter_stmt
     );
 end;
 $$ language plpgsql;
+
 
 
 -- diff_bundle_bundle_commits( bundle_table_a, bundle_table_b )
 --
 -- outer-joins the bundle tables of databases, one row per bundle.  the row also contains a json aggregate of every commit in that bundle.
 
-create or replace function diff_bundle_bundle_commits(
-    bundle_table_a meta.relation_id,
-    bundle_table_b meta.relation_id
+create or replace function bundle.remote_commits_diff(
+    remote_database_id uuid,
+    bundle_id uuid default null
 ) returns table (
     a_bundle_id uuid, a_name text, a_head_commit_id uuid, a_commits json,
     b_bundle_id uuid, b_name text, b_head_commit_id uuid, b_commits json
 )
 as $$
-select
-    a.id as a_bundle_id, a.name as a_name, a.head_commit_id as a_head_commit_id, a.commits as a_commits,
-    b.id as b_bundle_id, b.name as b_name, b.head_commit_id as b_head_commit_id, b.commits as b_commits
-    from bundle.bundle_commits_array( bundle_table_a ) a
-        full outer join bundle.bundle_commits_array( bundle_table_b ) b
-            on a.id = b.id
+declare
+    bundle_filter_stmt text;
+    remote_schema_name text;
+    remote_host text;
+begin
+    select schema_name, host from bundle.remote_database
+        where id = remote_database_id
+	into remote_schema_name, remote_host;
+
+    return query execute format('
+        select a.id as a_bundle_id, a.name as a_name, a.head_commit_id as a_head_commit_id, a.commits as a_commits,
+            b.id as b_bundle_id, b.name as b_name, b.head_commit_id as b_head_commit_id, b.commits as b_commits
+        from bundle.bundle_commits_array( meta.relation_id(''bundle'',''bundle''), %L ) a
+            full outer join bundle.bundle_commits_array( meta.relation_id (%L,''bundle''), %L) b
+                on a.id = b.id
+        ', bundle_id, remote_schema_name, bundle_id );
+end;
+$$ language plpgsql;
+
+
+
+create or replace function bundle.remote_commits_ahead( remote_database_id, bundle_id ) 
+returns bundle.commit
+as $$
+declare
+    source_schema_name text;
+    source_host text;
+    source_bundle_name text;
+    source_bundle_id uuid;
+begin
+    select schema_name, host from bundle.remote_database
+        where id = remote_database_id
+	into source_schema_name, source_host;
+    -- source
+    execute format ('select b.name, b.id from %1$I.bundle b where id=%2$L', source_schema_name, bundle_id) into source_bundle_name, source_bundle_id;
+    raise notice 'Cloning bundle % (%) from %...', source_bundle_name, source_bundle_id, source_host;
+
+    execute format ('select c.* from %1$I.bundle b join %1$I.commit c on c.bundle_id = b.id where b.id = %2$L and c.id not in (select c.id from bundle.commit c)',
+        remote
+
 $$ language sql;
 
 
-/*
 
-EARLIER ATTEMPTS AT GREATNESS:
-
--- remote_bundle_level_diff ()
---
--- compare the bundles in two bundle schemas, typically a local one and a
--- remote one.  returns all bundles present in either database.
-
-create or replace function remote_bundle_level_diff( local meta.relation_id, remote meta.relation_id )
-returns table (
-    local_id uuid, local_name text, local_head_commit_id uuid, local_commits_hash text[],
-    remote_id uuid, remote_name text, remote_head_commit_id uuid, remote_commits_hash text[]
-)
-as $$
-begin
-    return query execute format('
-        select
-            local.id as local_id,
-            local.name as local_name,
-            local.head_commit_id as local_head_commit_id,
-            array_agg(local_c.id::text) as local_commits_hash,
-        from %I.%I local
-            join %I.commit local_c
-
-
-            remote.id as remote_id,
-            remote.name as remote_name,
-            remote.head_commit_id as remote_head_commit_id,
-            array_agg(remote_c.id::text) as remote_commits_hash
-
-        from %I.%I local
-            full outer join %I.%I remote on remote.id = local.id
-            left join %I.commit local_c on local_c.bundle_id=local.id
-            left join %I.commit remote_c on remote_c.bundle_id=remote.id
-        group by local_id, local_name, local_head_commit_id, remote_id, remote_name, remote_head_commit_id
-        ',
-        (local::meta.schema_id).name, local.name,
-        (remote::meta.schema_id).name, remote.name,
-        (local::meta.schema_id).name,
-        (remote::meta.schema_id).name
-    );
-end;
-$$
-language plpgsql;
-
--- remote_commit_level_diff(bundle_name, schema1_name, schema2_name)
---
--- returns commits in remote but not local
-
-create type commit_diff as (
-    c1_id uuid,
-    c1_bundle_id uuid,
-    c1_message text,
-    c1_time timestamp,
-    c1_parent_id uuid,
-    c1_role_id meta.role_id,
-
-    c2_id uuid,
-    c2_bundle_id uuid,
-    c2_message text,
-    c2_time timestamp,
-    c2_parent_id uuid,
-    c2_role_id meta.role_id
-);
-
-create or replace function remote_commit_level_diff (
-    schema1_name text,
-    schema2_name text
-) returns setof commit_diff
-as $$
-begin
-    return query execute format('
-        select
-            c1.id as c1_id,
-            c1.bundle_id as c1_bundle_id,
-            c1.message as c1_message,
-            c1.time as c1_time,
-            c1.parent_id as c1_parent_id,
-            c1.role_id as c1_role_id,
-
-            c2.id as c2_id,
-            c2.bundle_id as c2_bundle_id,
-            c2.message as c2_message,
-            c2.time as c2_time,
-            c2.parent_id as c2_parent_id,
-            c2.role_id as c2_role_id
-
-        from %I.commit c1
-            full outer join %I.commit c2 on c1.id = c2.id and c1.bundle_id = c2.bundle_id
-        where c1.id is null or c2.id is null',
-        schema1_name,
-        schema2_name
-    );
-end;
-$$
-language plpgsql;
-*/
-
-
-
--- remote_clone ()
+-- remote_pull_bundle()
 --
 -- copy a repository from one bundle schema (typically a remote) to another (typically a local one)
-create or replace function remote_clone( remote_database_id uuid, bundle_id uuid ) -- source_schema_name text, dest_schema_name text )
+create or replace function bundle.remote_pull_bundle( remote_database_id uuid, bundle_id uuid ) -- source_schema_name text, dest_schema_name text )
 returns boolean as $$
 declare
     source_schema_name text;
@@ -280,13 +223,9 @@ declare
     source_bundle_name text;
     source_bundle_id uuid;
 begin
-    -- these used to be arguments, but now they're not.  we need to track remote_database_id explicitly.
     select schema_name, host from bundle.remote_database
         where id = remote_database_id
 	into source_schema_name, source_host;
-
-    -- dest_schema_name
-    select 'bundle' into dest_schema_name;
 
     -- source
     execute format ('select b.name, b.id from %1$I.bundle b where id=%2$L', source_schema_name, bundle_id) into source_bundle_name, source_bundle_id;
@@ -295,57 +234,57 @@ begin
     --------------- transfer --------------
     -- rowset
     raise notice '...rowset';
-    execute format ('insert into %2$I.rowset
+    execute format ('insert into bundle.rowset
         select r.* from %1$I.commit c
             join %1$I.rowset r on c.rowset_id = r.id
-        where c.bundle_id=%3$L', source_schema_name, dest_schema_name, bundle_id);
+        where c.bundle_id=%2$L', source_schema_name, bundle_id);
 
     -- rowset_row
     raise notice '...rowset_row';
     execute format ('
-        insert into %2$I.rowset_row
+        insert into bundle.rowset_row
         select rr.* from %1$I.commit c
             join %1$I.rowset r on c.rowset_id = r.id
             join %1$I.rowset_row rr on rr.rowset_id = r.id
-        where c.bundle_id=%3$L', source_schema_name, dest_schema_name, bundle_id);
+        where c.bundle_id=%2$L', source_schema_name, bundle_id);
 
     -- blob
     raise notice '...blob';
     execute format ('
-        insert into %2$I.blob
+        insert into bundle.blob
         select bb.hash, bb.value from %1$I._bundle_blob bb
-        where bb.bundle_id=%3$L', source_schema_name, dest_schema_name, bundle_id);
+        where bb.bundle_id=%2$L', source_schema_name, bundle_id);
 
     -- rowset_row_field
     raise notice '...rowset_row_field';
     execute format ('
-        insert into %2$I.rowset_row_field
+        insert into bundle.rowset_row_field
         select f.* from %1$I.commit c
             join %1$I.rowset r on c.rowset_id = r.id
             join %1$I.rowset_row rr on rr.rowset_id = r.id
             join %1$I.rowset_row_field f on f.rowset_row_id = rr.id
-        where c.bundle_id=%3$L', source_schema_name, dest_schema_name, bundle_id);
+        where c.bundle_id=%2$L', source_schema_name, bundle_id);
 
     -- bundle
     raise notice '...bundle';
-    execute format ('insert into %2$I.bundle (id, name)
+    execute format ('insert into bundle.bundle (id, name)
         select b.id, b.name from %1$I.bundle b
-        where b.id=%3$L', source_schema_name, dest_schema_name, bundle_id);
+        where b.id=%2$L', source_schema_name, bundle_id);
 
     -- commit
     raise notice '...commit';
     execute format ('
-        insert into %2$I.commit
+        insert into bundle.commit
         select c.* from %1$I.commit c
-        where c.bundle_id=%3$L', source_schema_name, dest_schema_name, bundle_id);
+        where c.bundle_id=%2$L', source_schema_name, bundle_id);
 
     -- bundle.head_commit_id
-    execute format ('update %2$I.bundle
+    execute format ('update bundle.bundle
         set head_commit_id = (
             select b.head_commit_id
             from %1$I.bundle b
-            where b.id=%3$L
-    ) where id=%3$L', source_schema_name, dest_schema_name, bundle_id);
+            where b.id=%2$L
+    ) where id=%2$L', source_schema_name, bundle_id);
 
     execute format ('insert into bundle.bundle_origin_remote (bundle_id, remote_database_id) values( %L, %L )', bundle_id, remote_database_id);
 
@@ -356,15 +295,38 @@ language plpgsql;
 
 
 
-/* optimization view for postgres_fdw */
+create or replace function bundle.remote_push_bundle( remote_database_id uuid, bundle_id uuid ) -- source_schema_name text, dest_schema_name text )
+returns boolean as $$
+declare
+    remote_schema_name text;
+    remote_host text;
+    source_bundle_name text;
+begin
+    
+    -- these used to be arguments, but now they're not.  we need to track remote_database_id explicitly.
+    select schema_name, host from bundle.remote_database
+        where id = remote_database_id
+	into remote_schema_name, remote_host;
 
-create or replace view _bundle_blob as
-select distinct on (b.id, bb.hash) b.id as bundle_id, bb.* from bundle b
-    join commit c on c.bundle_id = b.id
-    join rowset r on c.rowset_id = r.id
-    join rowset_row rr on rr.rowset_id = r.id
-    join rowset_row_field rrf on rrf.rowset_row_id = rr.id
-    join blob bb on bb.hash = rrf.value_hash;
+    select name from bundle.bundle where id = bundle_id
+    into source_bundle_name;
+
+    raise notice 'Pushing bundle % (%) from %...', source_bundle_name, bundle_id, remote_host;
+    raise notice '...bundle';
+    execute format ('insert into %1$I.bundle (id,name)
+        select b.id, b.name from bundle.bundle b
+        where b.id=%2$L', remote_schema_name, bundle_id);
+
+    perform remote_push_commits( remote_database_id, bundle_id );
+
+    raise notice '...updating bundle.head_commit_id';
+    execute format ('update bundle.bundle b
+        set head_commit_id=(select head_commit_id from %1$I.bundle b where b.id=%2$L)
+        where b.id=%2$L', remote_schema_name, bundle_id);
+    return true;
+end;
+$$
+language plpgsql;
 
 
 
@@ -375,8 +337,8 @@ select distinct on (b.id, bb.hash) b.id as bundle_id, bb.* from bundle b
  *
  */
 
-create or replace function remote_pull( remote_database_id uuid, bundle_id uuid )
-returns setof text as $$
+create or replace function bundle.remote_pull_commits( remote_database_id uuid, bundle_id uuid )
+returns boolean as $$
 declare
 	dest_schema_name text;
 	source_host text;
@@ -407,6 +369,10 @@ begin
                 and c.id not in (select id from bundle.commit where bundle_id = %2$L)
         ', source_schema_name, bundle_id)
         into new_commits_count, new_commit_ids;
+
+        if new_commits_count = 0 then 
+            new_commit_ids = quote_literal(false);
+        end if;
 
     -- notice
     raise notice 'Pulling % new commits for % (%) from %...', 
@@ -463,20 +429,22 @@ begin
             and c.id in (%4$s)',
         source_schema_name, dest_schema_name, bundle_id, new_commit_ids);
 
+    return true;
+
 end;
 $$ language plpgsql;
 
 
 
 /*
- * bundle.remote_pull
+ * bundle.remote_push_commits()
  *
  * transfer from remote all the commits that are not in the local repostiory for specified bundle
  *
  */
 
-create or replace function remote_push( remote_database_id uuid, bundle_id uuid )
-returns setof text as $$
+create or replace function bundle.remote_push_commits( remote_database_id uuid, bundle_id uuid )
+returns boolean as $$
 declare
 	dest_schema_name text;
 	remote_host text;
@@ -508,6 +476,11 @@ begin
         ', remote_schema_name, bundle_id)
         into new_commits_count, new_commit_ids;
 
+        if new_commits_count = 0 then 
+            new_commit_ids = quote_literal(false);
+        end if;
+
+
     -- notice
     raise notice 'Pushing % new commits for % (%) from %...', 
         new_commits_count, remote_bundle_name, remote_bundle_id, remote_host;
@@ -538,15 +511,16 @@ begin
     raise notice '...blob';
     execute format ('
         insert into %1$I.blob
-        select bb.hash, bb.value from bundle.bundle b
+        select distinct bb.hash, bb.value from bundle.bundle b
             join bundle.commit c on c.bundle_id = b.id
             join bundle.rowset r on c.rowset_id = r.id
             join bundle.rowset_row rr on rr.rowset_id = r.id
             join bundle.rowset_row_field rrf on rrf.rowset_row_id = rr.id
             join bundle.blob bb on rrf.value_hash = bb.hash
-            where b.id=%2$L',
+        where b.id=%2$L
+            and c.id in (%3$s)',
             -- and bb.hash not in (select all the hashes that aren't new.... optimization)
-        remote_schema_name, bundle_id);
+        remote_schema_name, bundle_id, new_commit_ids);
 
     -- rowset_row_field
     raise notice '...rowset_row_field';
@@ -568,5 +542,18 @@ begin
             and c.id in (%3$s)',
         remote_schema_name, bundle_id, new_commit_ids);
 
+    return true;
 end;
 $$ language plpgsql;
+
+
+
+/* optimization view for postgres_fdw */
+
+create or replace view _bundle_blob as
+select distinct on (b.id, bb.hash) b.id as bundle_id, bb.* from bundle.bundle b
+    join bundle.commit c on c.bundle_id = b.id
+    join bundle.rowset r on c.rowset_id = r.id
+    join bundle.rowset_row rr on rr.rowset_id = r.id
+    join bundle.rowset_row_field rrf on rrf.rowset_row_id = rr.id
+    join bundle.blob bb on bb.hash = rrf.value_hash;
