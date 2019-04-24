b8f4e3d7e06e9246e0e6c26eb85aeb29c8372108
Eric Hanson <eric@aquameta.com>
2019-04-24 14:14:30 +0000

    start of new documentation system

---
+++ src/pg-extension/documentation/000-datamodel.sql
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Documentation
+ * General Purpose Documentation System 
+ * 
+ * Copyriright (c) 2019 - Aquameta - http://aquameta.org/
+ ******************************************************************************/
+
+
+create schema documentation; -- TODO: remove this
+set search_path=documentation;
+
+
+
+------------------------------------------------------------------------------
+-- 1. DDL Documentation
+-- These are for documenting database objects, like schemas, tables, views,
+-- functions, etc.
+------------------------------------------------------------------------------
+
+create table "schema" (
+    id uuid not null default public.uuid_generate_v4() primary key,
+    schema_id meta.schema_id not null,
+    content text not null default ''
+);
+
+create table "table" (
+    id uuid not null default public.uuid_generate_v4() primary key,
+    relation_id meta.relation_id not null,
+    content text not null default ''
+);
