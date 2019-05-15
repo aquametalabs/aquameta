25ec4c0bde9a433e1d0bc67d5167a51d290b27ba
Eric Hanson <eric@aquameta.com>
2019-05-15 08:45:27 +0000

    docs: experimental documentation system schema

--- src/pg-extension/documentation/000-datamodel.sql
+++ src/pg-extension/documentation/000-datamodel.sql
@@ -17,6 +17,7 @@ set search_path=documentation;
 -- functions, etc.
 ------------------------------------------------------------------------------
 
+/*
 create table "schema" (
     id uuid not null default public.uuid_generate_v4() primary key,
     schema_id meta.schema_id not null,
@@ -26,5 +27,21 @@ create table "schema" (
 create table "table" (
     id uuid not null default public.uuid_generate_v4() primary key,
     relation_id meta.relation_id not null,
+    content text not null default ''
+);
+
+*/
+
+create table bundle_doc (
+    id uuid not null default public.uuid_generate_v4() primary key,
+    bundle_id uuid references bundle.bundle(id),
+    title text not null default '',
+    content text not null default ''
+);
+
+create table row_doc (
+    id uuid not null default public.uuid_generate_v4() primary key,
+    row_id meta.row_id not null,
+    title text not null default '',
     content text not null default ''
 );
