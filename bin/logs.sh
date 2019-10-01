b4a9bca42c430be1a555bd89fbc55cbd83b5ea6c
Eric Hanson <eric@aquameta.com>
2019-10-01 23:14:07 +0000

    new logs.sh script

--- bin/logs.sh
+++ bin/logs.sh
@@ -1,1 +1,2 @@
-{ tail -f /var/log/postgresql/postgresql-10-main.log /var/log/nginx/aquameta_db.*.log & journalctl -u aquameta.emperor.uwsgi.service -f;  }
+tail -f /var/log/postgresql/postgresql-10-main.log /var/log/nginx/aquameta_db.*.log
+journalctl -u aquameta.emperor.uwsgi.service -f
