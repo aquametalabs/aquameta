55c9e4f6eb465f3e4b922b6c364f90f8e90bbe42
Eric Hanson <eric@aquameta.com>
2019-12-03 12:46:47 +0000

    bundle: fix a couple of issues in the export.sql script

--- bundles-available/export.sql
+++ bundles-available/export.sql
@@ -7,24 +7,25 @@
 -- that user.
 
 -- core modules
-select bundle.bundle_export_csv('org.aquameta.core.endpoint','/home/eric/aquameta/bundles-available/org.aquameta.core.endpoint');
-select bundle.bundle_export_csv('org.aquameta.core.mimetypes','/home/eric/aquameta/bundles-available/org.aquameta.core.mimetypes');
-select bundle.bundle_export_csv('org.aquameta.core.semantics','/home/eric/aquameta/bundles-available/org.aquameta.core.semantics');
-select bundle.bundle_export_csv('org.aquameta.core.docs','/home/eric/aquameta/bundles-available/org.aquameta.core.docs');
-select bundle.bundle_export_csv('org.aquameta.core.ide','/home/eric/aquameta/bundles-available/org.aquameta.core.ide');
-select bundle.bundle_export_csv('org.aquameta.core.widget','/home/eric/aquameta/bundles-available/org.aquameta.core.widget');
+select bundle.bundle_export_csv('org.aquameta.core.endpoint','/opt/aquameta/bundles-available/org.aquameta.core.endpoint');
+select bundle.bundle_export_csv('org.aquameta.core.mimetypes','/opt/aquameta/bundles-available/org.aquameta.core.mimetypes');
+select bundle.bundle_export_csv('org.aquameta.core.semantics','/opt/aquameta/bundles-available/org.aquameta.core.semantics');
+select bundle.bundle_export_csv('org.aquameta.core.docs','/opt/aquameta/bundles-available/org.aquameta.core.docs');
+select bundle.bundle_export_csv('org.aquameta.core.ide','/opt/aquameta/bundles-available/org.aquameta.core.ide');
+select bundle.bundle_export_csv('org.aquameta.core.widget','/opt/aquameta/bundles-available/org.aquameta.core.widget');
 
 
 -- example modules
-select bundle.bundle_export_csv('org.aquameta.games.snake','/home/eric/aquameta/bundles-available/org.aquameta.games.snake');
+select bundle.bundle_export_csv('org.aquameta.games.snake','/opt/aquameta/bundles-available/org.aquameta.games.snake');
 
 -- user interface modules
-select bundle.bundle_export_csv('org.aquameta.ui.admin','/home/eric/aquameta/bundles-available/org.aquameta.ui.admin');
-select bundle.bundle_export_csv('org.aquameta.ui.auth','/home/eric/aquameta/bundles-available/org.aquameta.ui.auth');
-select bundle.bundle_export_csv('org.aquameta.ui.bundle','/home/eric/aquameta/bundles-available/org.aquameta.ui.bundle');
-select bundle.bundle_export_csv('org.aquameta.ui.dev','/home/eric/aquameta/bundles-available/org.aquameta.ui.dev');
-select bundle.bundle_export_csv('org.aquameta.ui.event','/home/eric/aquameta/bundles-available/org.aquameta.ui.event');
-select bundle.bundle_export_csv('org.aquameta.ui.fsm','/home/eric/aquameta/bundles-available/org.aquameta.ui.fsm');
-select bundle.bundle_export_csv('org.aquameta.ui.layout','/home/eric/aquameta/bundles-available/org.aquameta.ui.layout');
-select bundle.bundle_export_csv('org.aquameta.ui.tags','/home/eric/aquameta/bundles-available/org.aquameta.ui.tags');
-select bundle.bundle_export_csv('org.aquameta.templates.simple','/home/eric/aquameta/bundles-available/org.aquameta.templates.simple');
+select bundle.bundle_export_csv('org.aquameta.ui.admin','/opt/aquameta/bundles-available/org.aquameta.ui.admin');
+select bundle.bundle_export_csv('org.aquameta.ui.auth','/opt/aquameta/bundles-available/org.aquameta.ui.auth');
+select bundle.bundle_export_csv('org.aquameta.ui.bundle','/opt/aquameta/bundles-available/org.aquameta.ui.bundle');
+select bundle.bundle_export_csv('org.aquameta.ui.dev','/opt/aquameta/bundles-available/org.aquameta.ui.dev');
+select bundle.bundle_export_csv('org.aquameta.ui.event','/opt/aquameta/bundles-available/org.aquameta.ui.event');
+select bundle.bundle_export_csv('org.aquameta.ui.fsm','/opt/aquameta/bundles-available/org.aquameta.ui.fsm');
+select bundle.bundle_export_csv('org.aquameta.ui.layout','/opt/aquameta/bundles-available/org.aquameta.ui.layout');
+select bundle.bundle_export_csv('org.aquameta.ui.tags','/opt/aquameta/bundles-available/org.aquameta.ui.tags');
+select bundle.bundle_export_csv('org.aquameta.ui.p2p','/opt/aquameta/bundles-available/org.aquameta.ui.tags');
+select bundle.bundle_export_csv('org.aquameta.templates.simple','/opt/aquameta/bundles-available/org.aquameta.templates.simple');
