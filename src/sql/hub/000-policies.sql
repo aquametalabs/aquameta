9e6470bdd9f3dc21c5117e6e76961b3c7400ef0c
Eric Hanson <eric@aquameta.com>
2020-05-11 15:51:34 -0500

    hub: fix policies so only core bundles are visible

--- src/sql/hub/000-policies.sql
+++ src/sql/hub/000-policies.sql
@@ -11,10 +11,36 @@ other things to do:
 
 alter role anonymous password 'anonymous';
 
+
+/*
+ * bundle privileges
+ *
+ */
+
+-- base grant permissions
+grant usage on schema bundle to anonymous;
+grant select on all tables in schema bundle to anonymous;
+
+-- rls privileges
+alter table bundle.bundle enable row level security;
 create policy anonymous_core_bundles on bundle.bundle for select to anonymous
     using (name like 'org.aquameta.%');
 
-grant usage on schema bundle to anonymous;
-grant select on all tables in schema bundle to anonymous;
+/*
+grant select on table commit to anonymous;
+grant select on table rowset to anonymous;
+grant select on table rowset_row to anonymous;
+grant select on table rowset_row_field to anonymous;
+grant select on table blob to anonymous;
+grant select on table ignored_row to anonymous;
+grant select on table ignored_column to anonymous;
+grant select on table ignored_relation to anonymous;
+grant select on table ignored_schema to anonymous;
+*/
+
+/*
+ * ui privileges
+ *
+ */
 
 grant select on table widget.widget to anonymous;
