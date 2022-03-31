df6c7964d3b0ceb061aae0292915e82df73c653e
Eric Hanson <eric@aquameta.com>
2022-03-31 02:37:27 +0000

    IDE: latest stuff

---
+++ bundles/import.sh
@@ -0,0 +1,13 @@
+psql -c "select bundle.import_csv('org.aquameta.core.bootloader')";
+org.aquameta.core.bundle
+org.aquameta.core.endpoint
+org.aquameta.core.endpoint.tests
+org.aquameta.core.ide
+org.aquameta.core.mimetypes
+org.aquameta.core.semantics
+org.aquameta.core.widget
+org.aquameta.games.snake
+org.aquameta.ui.auth
+org.aquameta.ui.fsm
+org.aquameta.ui.layout
+org.aquameta.ui.tags
