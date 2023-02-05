553e4da684e5aee836fb93ebd701d17c95d53e2f
Eric Hanson <eric@aquameta.com>
2023-02-05 18:59:51 +0000

    updates to the build-v2.sh script

--- extensions/build-v2.sh
+++ extensions/build-v2.sh
@@ -1,15 +1,14 @@
 reset
+
+# import the flattened stuff
 dropdb flat
 createdb flat
-
-# import the flat stuff
 cat meta/flat/0*.sql|psql flat
 cat bundle/0*.sql|psql flat
 cd ../
 
 # init the non-flat stuff
 ./aquameta -c conf/flat.toml
-
 
 # import old private bundles
 # cd ~orchestrator/aquameta/bundles.private
@@ -23,10 +22,11 @@ cat bundle/migrate_flat.sql|psql flat
 psql -c 'select bundle2.bundle_export_csv(b.name, bc.directory) from bundle2.bundle b join bundle2.bundle_csv bc on bc.bundle_id = b.id;' flat
 
 # convert meta2 to meta, bundle2 to bundle
-# sed -i 's/meta2/meta/g' meta/flat/0*.sql bundle/0*.sql endpoint/0*.sql
-# sed -i 's/bundle2/bundle/g' meta/flat/0*.sql bundle/0*.sql endpoint/0*.sql
+# sed -i 's/meta2/meta/g' meta/flat/0*.sql bundle/0*.sql
+# sed -i 's/bundle2/bundle/g' meta/flat/0*.sql bundle/0*.sql
 
-# then repeat the "import the flat" step
-# then swap out main.go for main.go.no-install
+# repeat the "import the flat" step
+# cat bundle/0*.sql|psql flat
+# swap out main.go for main.go.no-install
 
 
