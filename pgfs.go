711afdc6b4cfdee01645b35b9d34102eb8cfad53
Eric Hanson <eric@aquameta.com>
2023-02-09 18:40:43 +0000

    pgfs: oops, add pgfs.go

---
+++ pgfs.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "context"
+    // "log"
+    "os"
+    "syscall"
+
+//    "github.com/jackc/pgx/v4/pgxpool"
+//    "github.com/lib/pq"
+
+    "bazil.org/fuse"
+    "bazil.org/fuse/fs"
+
+)
+
+// FS implements the hello world file system.
+type FS struct{
+
+}
+
+func (FS) Root() (fs.Node, error) {
+	return Dir{}, nil
+}
+
+// Dir implements both Node and Handle for the root directory.
+type Dir struct{}
+
+func (Dir) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = 1
+	a.Mode = os.ModeDir | 0o555
+	return nil
+}
+
+func (Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	if name == "hello" {
+		return File{}, nil
+	}
+	return nil, syscall.ENOENT
+}
+
+var dirDirs = []fuse.Dirent{
+	{Inode: 2, Name: "hello", Type: fuse.DT_File},
+}
+
+func (Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	return dirDirs, nil
+}
+
+// File implements both Node and Handle for the hello file.
+type File struct{}
+
+const greeting = "hello, world\n"
+
+func (File) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = 2
+	a.Mode = 0o444
+	a.Size = uint64(len(greeting))
+	return nil
+}
+
+func (File) ReadAll(ctx context.Context) ([]byte, error) {
+	return []byte(greeting), nil
+}
