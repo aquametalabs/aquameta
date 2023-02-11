5d123878aba8223bb83d03386ccdaa3036f75b19
Eric Hanson <eric@aquameta.com>
2023-02-11 15:25:01 +0000

    pgfs: stubs for SchemaDir, TableDir, RowDir and FieldFile

--- pgfs.go
+++ pgfs.go
@@ -4,11 +4,11 @@ import (
     "context"
     "log"
     "os"
-    "syscall"
+    "fmt"
 
     "github.com/jackc/pgx/v4/pgxpool"
     // "github.com/jackc/pgx/v4"
-    // "github.com/lib/pq"
+    "github.com/lib/pq"
 
     "bazil.org/fuse"
     "bazil.org/fuse/fs"
@@ -21,8 +21,9 @@ type FS struct{
 }
 
 func (f FS) Root() (fs.Node, error) {
-	return Dir{f}, nil
+    return Dir{f}, nil
 }
+
 
 // Dir implements both Node and Handle for the root directory.
 type Dir struct{
@@ -30,68 +31,232 @@ type Dir struct{
 }
 
 func (Dir) Attr(ctx context.Context, a *fuse.Attr) error {
-	a.Inode = 1
-	a.Mode = os.ModeDir | 0o555
-	return nil
+    a.Inode = 1
+    a.Mode = os.ModeDir | 0o555
+    return nil
 }
 
-func (Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
-	if name == "hello" {
-		return File{}, nil
-	}
-	return nil, syscall.ENOENT
+func (d Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+    return SchemaDir{d.fs}, nil
+    // return nil, syscall.ENOENT
 }
-
-/*
-var dirDirs = []fuse.Dirent{
-	{Inode: 2, Name: "hello", Type: fuse.DT_File},
-}
-*/
 
 func (d Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
-    rows, err := d.fs.dbpool.Query(context.Background(), "select name from meta.schema")
-	if err != nil {
-		log.Fatal("Error querying database: ", err)
-	}
-	defer rows.Close()
+    q := fmt.Sprintf("select name from meta.schema")
+    rows, err := d.fs.dbpool.Query(context.Background(), q)
 
-	var dirDirs []fuse.Dirent
-	for rows.Next() {
-		var name string
+    if err != nil {
+        log.Fatal("Error querying database: ", err)
+    }
+    defer rows.Close()
 
-		err := rows.Scan(&name)
-		if err != nil {
-			log.Fatal("Error scanning row", err)
-			continue
-		}
+    var dirDirs []fuse.Dirent
+    for rows.Next() {
+        var name string
 
-		log.Println("Schema:", name)
-		dirDirs = append(dirDirs, fuse.Dirent{
-			Inode: 2,
-			Name: name,
-			Type: fuse.DT_File,
-		})
-	}
+        err := rows.Scan(&name)
+        if err != nil {
+            log.Fatal("Error scanning row", err)
+            continue
+        }
 
-	if rows.Err() != nil {
-		log.Fatal("Error iterating rows", rows.Err())
-	}
+        // log.Println("Schema:", name)
+        dirDirs = append(dirDirs, fuse.Dirent{
+            Inode: 2,
+            Name: name,
+            Type: fuse.DT_Dir,
+        })
+    }
 
-	return dirDirs, nil
+    if rows.Err() != nil {
+        log.Fatal("Error iterating rows", rows.Err())
+    }
+
+    return dirDirs, nil
 }
 
+
+
+// SchemaDir
+type SchemaDir struct{
+    fs FS
+}
+
+func (SchemaDir) Attr(ctx context.Context, a *fuse.Attr) error {
+    a.Inode = 1
+    a.Mode = os.ModeDir | 0o555
+    return nil
+}
+
+func (d SchemaDir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+        return TableDir{d.fs}, nil // File{}, nil
+}
+
+func (d SchemaDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+     q := fmt.Sprintf("select name from meta.relation where schema_name=%s and primary_key_column_ids is not null",
+         pq.QuoteLiteral("bundle"))
+    rows, err := d.fs.dbpool.Query(context.Background(), q)
+
+    if err != nil {
+        log.Fatal("Error querying database: ", err)
+    }
+    defer rows.Close()
+
+    var dirDirs []fuse.Dirent
+    for rows.Next() {
+        var name string
+
+        err := rows.Scan(&name)
+        if err != nil {
+            log.Fatal("Error scanning row", err)
+            continue
+        }
+
+        // log.Println("Schema:", name)
+        dirDirs = append(dirDirs, fuse.Dirent{
+            Inode: 2,
+            Name: name,
+            Type: fuse.DT_Dir,
+        })
+    }
+
+    if rows.Err() != nil {
+        log.Fatal("Error iterating rows", rows.Err())
+    }
+
+    return dirDirs, nil
+}
+
+
+
+
+
+
+// TableDir
+type TableDir struct{
+    fs FS
+}
+
+func (TableDir) Attr(ctx context.Context, a *fuse.Attr) error {
+    a.Inode = 1
+    a.Mode = os.ModeDir | 0o555
+    return nil
+}
+
+func (d TableDir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+        return RowDir{}, nil // File{}, nil
+}
+
+func (d TableDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+     q := fmt.Sprintf("select %s as pk_value from %s.%s",
+         pq.QuoteIdentifier("id"),
+         pq.QuoteIdentifier("bundle"),
+         pq.QuoteIdentifier("commit"))
+
+    rows, err := d.fs.dbpool.Query(context.Background(), q)
+    if err != nil {
+        log.Fatal("Error querying database: ", err)
+    }
+    defer rows.Close()
+
+    var dirDirs []fuse.Dirent
+    for rows.Next() {
+        var pk_value string
+
+        err := rows.Scan(&pk_value)
+        if err != nil {
+            log.Fatal("Error scanning row", err)
+            continue
+        }
+
+        // log.Println("Primary Key:", pk_value)
+        dirDirs = append(dirDirs, fuse.Dirent{
+            Inode: 2,
+            Name: pk_value,
+            Type: fuse.DT_Dir,
+        })
+    }
+
+    if rows.Err() != nil {
+        log.Fatal("Error iterating rows", rows.Err())
+    }
+
+    return dirDirs, nil
+}
+
+
+
+
+
+
+// RowDir
+type RowDir struct{
+    fs FS
+}
+
+func (RowDir) Attr(ctx context.Context, a *fuse.Attr) error {
+    a.Inode = 1
+    a.Mode = os.ModeDir | 0o555
+    return nil
+}
+
+func (d RowDir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+        return FieldFile{d.fs}, nil
+}
+
+func (d RowDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+     q := fmt.Sprintf("select name as column_name from meta.column where schema_name=%s and relation_name=%s",
+         pq.QuoteIdentifier("bundle"),
+         pq.QuoteIdentifier("commit"))
+    rows, err := d.fs.dbpool.Query(context.Background(), q)
+
+    if err != nil {
+        log.Fatal("Error querying database: ", err)
+    }
+    defer rows.Close()
+
+    var dirDirs []fuse.Dirent
+    for rows.Next() {
+        var column_name string
+
+        err := rows.Scan(&column_name)
+        if err != nil {
+            log.Fatal("Error scanning row", err)
+            continue
+        }
+
+        // log.Println("Schema:", column_name)
+        dirDirs = append(dirDirs, fuse.Dirent{
+            Inode: 2,
+            Name: column_name,
+            Type: fuse.DT_File,
+        })
+    }
+
+    if rows.Err() != nil {
+        log.Fatal("Error iterating rows", rows.Err())
+    }
+
+    return dirDirs, nil
+}
+
+
+
+
 // File implements both Node and Handle for the hello file.
-type File struct{}
+type FieldFile struct{
+    fs FS
+}
 
 const greeting = "hello, world\n"
 
-func (File) Attr(ctx context.Context, a *fuse.Attr) error {
-	a.Inode = 2
-	a.Mode = 0o444
-	a.Size = uint64(len(greeting))
-	return nil
+func (FieldFile) Attr(ctx context.Context, a *fuse.Attr) error {
+    a.Inode = 2
+    a.Mode = 0o444
+    a.Size = uint64(len(greeting))
+    return nil
 }
 
-func (File) ReadAll(ctx context.Context) ([]byte, error) {
-	return []byte(greeting), nil
+func (FieldFile) ReadAll(ctx context.Context) ([]byte, error) {
+    return []byte(greeting), nil
 }
