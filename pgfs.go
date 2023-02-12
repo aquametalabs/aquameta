0ca4e11c8c0c217502c3ed56ed2be172e91db874
Eric Hanson <eric@aquameta.com>
2023-02-12 00:13:22 +0000

    pgfs: read-only mostly working

--- pgfs.go
+++ pgfs.go
@@ -34,12 +34,22 @@ func (Dir) Attr(ctx context.Context, a *fuse.Attr) error {
     a.Inode = 1
     a.Uid = uint32(syscall.Geteuid())
     a.Gid = uint32(syscall.Getegid())
-    a.Mode = os.ModeDir | 0o400
+    a.Mode = os.ModeDir | 0o500
     return nil
 }
 
 func (d Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
-    return SchemaDir{d.fs}, nil
+    var exists bool
+    q := fmt.Sprintf("select exists(select 1 from meta.schema where name=%s)", pq.QuoteLiteral(name))
+    err := d.fs.dbpool.QueryRow(context.Background(), q).Scan(&exists)
+    if err != nil {
+        log.Fatal("Error querying database: ", err)
+    }
+    if exists {
+        return SchemaDir{d.fs, name}, nil
+    }
+    return nil, fuse.ENOENT
+
 }
 
 func (d Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
@@ -77,27 +87,44 @@ func (d Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 }
 
 
-
+//
 // SchemaDir
+//
+
 type SchemaDir struct{
     fs FS
+    schema_name string
 }
 
-func (SchemaDir) Attr(ctx context.Context, a *fuse.Attr) error {
+func (d SchemaDir) Attr(ctx context.Context, a *fuse.Attr) error {
     a.Inode = 1
     a.Uid = uint32(syscall.Geteuid())
     a.Gid = uint32(syscall.Getegid())
-    a.Mode = os.ModeDir | 0o400
+    a.Mode = os.ModeDir | 0o500
     return nil
 }
 
 func (d SchemaDir) Lookup(ctx context.Context, name string) (fs.Node, error) {
-        return TableDir{d.fs}, nil
+    var pk_column_name string
+    q := fmt.Sprintf("select (primary_key_column_ids[1]).name as pk_column_name from meta.relation where schema_name=%s and name=%s and primary_key_column_ids is not null",
+		pq.QuoteLiteral(d.schema_name),
+		pq.QuoteLiteral(name))
+
+    err := d.fs.dbpool.QueryRow(context.Background(), q).Scan(&pk_column_name)
+    if err != nil {
+        log.Fatal("Error in SchemaDir Lookup: ", err)
+    }
+
+    if pk_column_name != "" {
+        return TableDir{d.fs, d.schema_name, name, pk_column_name}, nil
+    } else {
+        return nil, fuse.ENOENT
+    }
 }
 
 func (d SchemaDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
      q := fmt.Sprintf("select name from meta.relation where schema_name=%s and primary_key_column_ids is not null",
-         pq.QuoteLiteral("bundle"))
+         pq.QuoteLiteral(d.schema_name))
     rows, err := d.fs.dbpool.Query(context.Background(), q)
 
     if err != nil {
@@ -131,32 +158,47 @@ func (d SchemaDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 }
 
 
-
-
-
-
+//
 // TableDir
+//
+
 type TableDir struct{
     fs FS
+    schema_name string
+    table_name string
+    pk_column_name string
 }
 
 func (TableDir) Attr(ctx context.Context, a *fuse.Attr) error {
     a.Inode = 1
     a.Uid = uint32(syscall.Geteuid())
     a.Gid = uint32(syscall.Getegid())
-    a.Mode = os.ModeDir | 0o400
+    a.Mode = os.ModeDir | 0o500
     return nil
 }
 
 func (d TableDir) Lookup(ctx context.Context, name string) (fs.Node, error) {
-        return RowDir{d.fs}, nil
+    var exists bool
+    q := fmt.Sprintf("select exists(select 1 from %s.%s where %s=%s)",
+        pq.QuoteIdentifier(d.schema_name),
+        pq.QuoteIdentifier(d.table_name),
+        pq.QuoteIdentifier(d.pk_column_name),
+        pq.QuoteLiteral(name))
+    err := d.fs.dbpool.QueryRow(context.Background(), q).Scan(&exists)
+    if err != nil {
+        log.Fatal("Error querying database: ", err)
+    }
+    if exists {
+        return RowDir{d.fs, d.schema_name, d.table_name, d.pk_column_name, name}, nil
+    }
+    return nil, fuse.ENOENT
 }
 
 func (d TableDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
      q := fmt.Sprintf("select %s as pk_value from %s.%s",
-         pq.QuoteIdentifier("id"),
-         pq.QuoteIdentifier("bundle"),
-         pq.QuoteIdentifier("commit"))
+         pq.QuoteIdentifier(d.pk_column_name),
+         pq.QuoteIdentifier(d.schema_name),
+         pq.QuoteIdentifier(d.table_name))
 
     rows, err := d.fs.dbpool.Query(context.Background(), q)
     if err != nil {
@@ -192,29 +234,34 @@ func (d TableDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 
 
 
-
-
+//
 // RowDir
+//
+
 type RowDir struct{
     fs FS
+    schema_name string
+    table_name string
+    pk_column_name string
+    pk_value string
 }
 
 func (RowDir) Attr(ctx context.Context, a *fuse.Attr) error {
     a.Inode = 1
     a.Uid = uint32(syscall.Geteuid())
     a.Gid = uint32(syscall.Getegid())
-    a.Mode = os.ModeDir | 0o400
+    a.Mode = os.ModeDir | 0o500
     return nil
 }
 
 func (d RowDir) Lookup(ctx context.Context, name string) (fs.Node, error) {
-        return FieldFile{d.fs}, nil
+        return FieldFile{d.fs, d.schema_name, d.table_name, name, d.pk_column_name, d.pk_value}, nil
 }
 
 func (d RowDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
      q := fmt.Sprintf("select name as column_name from meta.column where schema_name=%s and relation_name=%s",
-         pq.QuoteLiteral("bundle"),
-         pq.QuoteLiteral("commit"))
+         pq.QuoteLiteral(d.schema_name),
+         pq.QuoteLiteral(d.table_name))
     rows, err := d.fs.dbpool.Query(context.Background(), q)
 
     if err != nil {
@@ -248,66 +295,62 @@ func (d RowDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
 }
 
 
+//
+// FieldFile
+//
 
-
-// File implements both Node and Handle for the hello file.
 type FieldFile struct{
     fs FS
+    schema_name string
+    table_name string
+    column_name string
+    pk_column_name string
+    pk_value string
 }
 
 
-
-const greeting = "hello, world\n"
-
 func (ff FieldFile) Attr(ctx context.Context, a *fuse.Attr) error {
-    var length int
+    var octet_length int
 
-    q := fmt.Sprintf("select octet_length(%s) as length from %s.%s where %s = %s",
-         pq.QuoteIdentifier("message"), // column name
-         pq.QuoteIdentifier("bundle"), // bundle_name
-         pq.QuoteIdentifier("commit"), // schema_name
-         pq.QuoteIdentifier("id"), // pk_column_name
-         pq.QuoteLiteral("fdf62c69-544b-4f9a-ab2e-dc5a64cb2d44")) // pk_value
+    q := fmt.Sprintf("select octet_length(%s::text)::integer as octet_length from %s.%s where %s = %s",
+         pq.QuoteIdentifier(ff.column_name),
+         pq.QuoteIdentifier(ff.schema_name),
+         pq.QuoteIdentifier(ff.table_name),
+         pq.QuoteIdentifier(ff.pk_column_name),
+         pq.QuoteLiteral(ff.pk_value))
 
-    err := ff.fs.dbpool.QueryRow(context.Background(), q).Scan(&length)
+    // fmt.Println(q)
+
+    err := ff.fs.dbpool.QueryRow(context.Background(), q).Scan(&octet_length)
 
     if err != nil {
         log.Fatal("Error querying database: ", err)
     }
 
     a.Inode = 2
-    a.Size = uint64(length)
-
-	// Set owner to current user
+    a.Size = uint64(octet_length)
     a.Uid = uint32(syscall.Geteuid())
     a.Gid = uint32(syscall.Getegid())
-
-	// Set the file mode to read-only
-	a.Mode = a.Mode | syscall.S_IRUSR | syscall.S_IRGRP
-	a.Mode = a.Mode &^ (syscall.S_IWUSR | syscall.S_IWGRP)
-
-
+    a.Mode = 0o644
 
     return nil
 }
 
 func (ff FieldFile) ReadAll(ctx context.Context) ([]byte, error) {
     var content string
-    var length int
 
-    q := fmt.Sprintf("select %s as content, octet_length(%s) as length from %s.%s where %s = %s",
-         pq.QuoteIdentifier("message"), // column name
-         pq.QuoteIdentifier("message"), // column name
-         pq.QuoteIdentifier("bundle"), // bundle_name
-         pq.QuoteIdentifier("commit"), // schema_name
-         pq.QuoteIdentifier("id"), // pk_column_name
-         pq.QuoteLiteral("fdf62c69-544b-4f9a-ab2e-dc5a64cb2d44")) // pk_value
+    q := fmt.Sprintf("select %s::text as content from %s.%s where %s = %s",
+         pq.QuoteIdentifier(ff.column_name),
+         pq.QuoteIdentifier(ff.schema_name),
+         pq.QuoteIdentifier(ff.table_name),
+         pq.QuoteIdentifier(ff.pk_column_name),
+         pq.QuoteLiteral(ff.pk_value))
 
-    err := ff.fs.dbpool.QueryRow(context.Background(), q).Scan(&content, &length)
+    err := ff.fs.dbpool.QueryRow(context.Background(), q).Scan(&content)
 
     if err != nil {
         log.Fatal("Error querying database: ", err)
     }
 
-    return []byte(greeting), nil
+    return []byte(content), nil
 }
