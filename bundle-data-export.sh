72730c932ba82174169c156591f186f84f4b2aa9
Eric Hanson <eric@aquameta.com>
2025-07-05 15:03:23 -0600

    fix bundle-data-export.sh script to exclude SET commmands (breaking pg15)

--- bundle-data-export.sh
+++ bundle-data-export.sh
@@ -1,4 +1,5 @@
 
-echo 'begin;' > bundle-data-export.sql
-pg_dump --schema=bundle --data-only aquameta >> bundle-data-export.sql
+echo 'begin;' > tmp.sql
+pg_dump --schema=bundle --data-only aquameta >> tmp.sql
+sed '1,/^SET row_security = off;$/d' tmp.sql > bundle-data-export.sql
 echo 'commit;' >> bundle-data-export.sql
